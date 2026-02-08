b017ab77193521e5338e871fe1e8e4e249000259
Nomer Icban Israel <46095783+nmrisrl11@users.noreply.github.com>
2026-02-08 14:05:26 +0800

    Setup folder layers
    - Setup models, routes, and controllers

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,46 @@
+import { User } from "../models/user.model.js";
+
+const registerUser = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+
+        if(!username || !email || !password) {
+            return res.status(400).json({
+                message: "All fields are required!"
+            });
+        }
+
+        const existing = await User.findOne({ email: email.toLowerCase() })
+
+        if(existing) {
+            return res.status(400).json({
+                message: "Email already exists!"
+            })
+        }
+
+        const user = await User.create({
+            username,
+            email: email.toLowerCase(),
+            password,
+            loggedIn: false
+        })
+
+        res.status(201).json({
+            message: "User registered",
+            user: { 
+                id: user._id,
+                email: user.email,
+                username: user.username
+             }
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal server error", 
+            error: error.message
+        })
+    }
+}
+
+export {
+    registerUser
+}
