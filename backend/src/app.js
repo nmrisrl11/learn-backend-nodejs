982bd1e02f096f4b89970387458ffe6aba4dba24
Nomer Icban Israel <46095783+nmrisrl11@users.noreply.github.com>
2026-02-09 22:24:06 +0800

    Updated app.js and user route

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,9 @@
 import express from "express";
+import useRouter from "./routes/user.route.js";
 
 const app = express();
+
+app.use(express.json());
+app.use("/api/v1/users", useRouter);
 
 export default app;
