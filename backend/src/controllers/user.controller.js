e5390f3314a06238b3abf0a894b9bb9c75f2ad83
Nomer Icban Israel <46095783+nmrisrl11@users.noreply.github.com>
2026-02-10 18:47:38 +0800

    Login Route and Controller
    - Added login route and controller
    - Installed bcrypt
    - Hashing of password with bcrypt

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -41,6 +41,41 @@ const registerUser = async (req, res) => {
     }
 }
 
-export {
-    registerUser
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({
+            email: email.toLowerCase()
+        });
+
+        if(!user) return res.status(400).json({
+            message: "User not found!"
+        });
+
+        const isMatch = await user.comparePassword(password);
+
+        if(!isMatch) return res.status(400).json({
+            message: "Invalid credentials!"
+        })
+
+        res.status(200).json({
+            message: "User logged in!",
+            user: {
+                id: user.id,
+                email: user.email,
+                username: user.username
+            }
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal server error", 
+            error: error.message
+        })
+    }
 }
+
+export {
+    registerUser,
+    loginUser
+};
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
     {
@@ -29,5 +30,15 @@ const userSchema = new Schema(
         timestamps: true
     }
 )
+
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
 
 export const User = mongoose.model("User", userSchema);
