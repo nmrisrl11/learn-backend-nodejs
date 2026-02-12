8640cfb1ee48ae040cac1475b1d1ca2571489732
Nomer Icban Israel <46095783+nmrisrl11@users.noreply.github.com>
2026-02-12 21:49:45 +0800

    Logout Route and Controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -75,7 +75,31 @@ const loginUser = async (req, res) => {
     }
 }
 
+const logoutUser = async (req, res) => {
+    try {
+        const { email } = req.body;
+
+        const user = await User.findOne({
+            email: email.toLowerCase()
+        })
+
+        if(!user) return res.status(404).json({
+            message: "User not found!"
+        });
+
+        res.status(200).json({
+            message: "Logout successful"
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal server error", 
+            error: error.message
+        })
+    }
+}
+
 export {
     registerUser,
-    loginUser
+    loginUser,
+    logoutUser
 };
